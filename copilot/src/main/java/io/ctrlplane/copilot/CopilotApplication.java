448c8048f4a0bd41f6df1ea5fcebd0285e3fbfd9
Evan Kwisnek <evankwisnek@gmail.com>
2022-04-11 20:15:23 -0400

    initialized copilot spring app

---
+++ copilot/src/main/java/io/ctrlplane/copilot/CopilotApplication.java
@@ -0,0 +1,13 @@
+package io.ctrlplane.copilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotApplication.class, args);
+	}
+
+}
---
+++ copilot/src/test/java/io/ctrlplane/copilot/CopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ctrlplane.copilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
